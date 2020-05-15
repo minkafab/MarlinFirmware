0b466a046501d9aee18ac572a27064160daae8d1
chezzandyto <andres_toapanta@hotmail.com>
2020-05-14 22:44:16 -0500

    habilitar carga y descarga de filamento en extrusor prusa config_adv.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1058,7 +1058,7 @@
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
 #define INVERT_X_DIR false
 #define INVERT_Y_DIR false
-#define INVERT_Z_DIR true
+#define INVERT_Z_DIR false
 
 // @section extruder
 
