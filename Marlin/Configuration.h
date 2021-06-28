2ce4a70d8922d5ab38b20a7b5c746887f2ac25d9
chezzandyto <andres_toapanta@hotmail.com>
2021-06-28 11:04:37 -0500

    correcion de area de impresion

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1356,15 +1356,15 @@
 
 // The size of the print bed
 #define X_BED_SIZE 220
-#define Y_BED_SIZE 220
+#define Y_BED_SIZE 218
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS 0
-#define Y_MIN_POS 0
+#define X_MIN_POS -2
+#define Y_MIN_POS -8
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 230
+#define Z_MAX_POS 228
 
 /**
  * Software Endstops
