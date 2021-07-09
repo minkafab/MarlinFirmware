e9e35445769bef98ebc45d137dff1ae6d3e90d9f
chezzandyto <andres_toapanta@hotmail.com>
2021-07-09 11:33:41 -0500

    offsets para MKF PRO oficina

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1380,16 +1380,16 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 220
+#define X_BED_SIZE 210
 #define Y_BED_SIZE 218
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -2
-#define Y_MIN_POS -8
+#define X_MIN_POS 0
+#define Y_MIN_POS 0
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 228
+#define Z_MAX_POS 222
 
 /**
  * Software Endstops
