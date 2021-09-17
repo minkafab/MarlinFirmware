4a1795b8442d2b8b9b930ae855c1fea8ef7e137c
chezzandyto <andres_toapanta@hotmail.com>
2021-09-17 15:24:53 -0500

    ventialdor maximo al 70%

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -502,8 +502,8 @@
  *
  * Define one or both of these to override the default 0-255 range.
  */
-//#define FAN_MIN_PWM 50
-//#define FAN_MAX_PWM 128
+#define FAN_MIN_PWM 0
+#define FAN_MAX_PWM 170
 
 /**
  * FAST PWM FAN Settings
